10e0cb09a896d45a5f7c8de8cc48b191ed60e448
Lucas Rosa Rodrigues <lcs-rodrigues@hotmail.com>
2020-08-18 07:33:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ const fornecedores = require("./Routers/Fornecedor")
 app.use("/fornecedores",fornecedores)
 
 app.listen(8081,function(){
-    console.log("servidor online")
+    console.log("servidor online na porta 8081")
 })
